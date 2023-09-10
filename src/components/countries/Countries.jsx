e8da54c31d9fdacaffdef595cebf040f58003384
tanjib10 <radiulmd@gmail.com>
2023-09-11 02:55:13 +0600

    added some features

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -4,18 +4,35 @@ import Country from "../country/country";
 import './countries.css'
 const Countries = () => {
    const  [countries, setCountries] = useState([]);
+   const [visitedCountries, setVisitedCountries] = useState([])
    useEffect(() => {
       fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
       .then(data => setCountries(data))
 
    },[])
+  const handleVisitedCountries = (country) => {
+     const newArray = [...visitedCountries,country]
+     setVisitedCountries(newArray)
+  }
+
    return (
       <div>
          <h3>Countries: {countries.length}</h3>
+         <div>
+            <h5>Visited countries : {visitedCountries.length}</h5>
+            <ul>
+               {
+                  visitedCountries.map(country => <li>{country.name.common}</li>)
+               }
+            </ul>
+         </div>
          <div className="countries">
              {
-            countries.map(country => <Country country={country}></Country>)
+            countries.map(country => <Country country={country}
+            handleVisitedCountries={() => handleVisitedCountries(country)}
+            >
+            </Country>)
          }
             </div> 
         
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './country.css';
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries }) => {
    const {name,flags,area,population} = country
+   const [visited,setVisited] = useState(false)
+   const handleVisited = () => {
+      setVisited(!visited)
+   }
    return (
-      <div className="country" >
-         <h5>Country Name : {name.common} </h5>
+      <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+         <h5 style={{color : 'steelblue'}}>Country Name : {name.common} </h5>
          <img className='image' src={flags.png} alt="" />
-         <p>area : {area}</p>
-         <p>population : {population}</p>
+         <p>area : {area} km <sup>2</sup></p>
+         <p>population: {population} </p>
+         <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+         <button onClick={handleVisitedCountries}>Mark as visited</button>
       </div>
    );
 };
